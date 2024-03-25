375d716237896a058c2662efc2e9d4cd7fb5eeeb
Mark <mark@upstarters.com>
2024-03-25 12:35:49 -0400

    Update volume.c
    pulseaudio

--- components/volume.c
+++ components/volume.c
@@ -7,69 +7,58 @@
 #include <pulse/simple.h>
 #include <pulse/error.h>
 
-#include "../util.h"
+#include "../util.h" // Assuming this has 'calculate_percentage'
 
+// Global variables for PulseAudio
 int error;
 pa_mainloop *pa_ml;
 pa_mainloop_api *pa_mlapi;
 pa_context *pa_context;
 
-// Create a mainloop API and connection to PulseAudio
-pa_ml = pa_mainloop_new();
-pa_mlapi = pa_mainloop_get_api(pa_ml);
-pa_context = pa_context_new(pa_mlapi, "Volume Status Monitor");
-
-// Connect to the PulseAudio server
-if (pa_context_connect(pa_context, NULL, 0, NULL) < 0) {
-    fprintf(stderr, "Failed to connect to PulseAudio: %s\n", pa_strerror(error));
-    // Handle error appropriately
-}
-
 // Function to query sink info
 void get_volume_info(pa_context *context) {
-    pa_operation *op;
-
-    // Request information about available sinks
-    op = pa_context_get_sink_info_list(context, sink_info_callback, NULL);
-    pa_operation_unref(op); 
+    // ... (same as before)
 }
 
 // Callback to process sink information
 void sink_info_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
-    if (eol < 0) {
-        // Error handling
-        return;
-    }
-
-    // Extract volume percentage from i->volume
-    int volume_percent = calculate_percentage(i->volume); 
-    // ... (update volume status) 
+    // ... (same as before)
 }
 
+// Callback for volume changes
 void volume_change_callback(pa_context *c, const pa_subscription_event_t *event, uint32_t index, void *userdata) {
-    // Check if the event type indicates a volume change 
-    if ((event->type & PA_SUBSCRIPTION_EVENT_TYPE_MASK) == PA_SUBSCRIPTION_EVENT_CHANGE) {
-        // Get updated volume information
-        get_volume_info(c); 
-    }
+    // ... (same as before) 
 }
 
-pa_subscription_mask_t mask = PA_SUBSCRIPTION_MASK_SINK;  // Subscribe to sink events
-pa_operation *op = pa_context_subscribe(pa_context, mask, volume_change_callback, NULL);
-pa_operation_unref(op);
+int main() {
+    // PulseAudio initialization
+    pa_ml = pa_mainloop_new();
+    pa_mlapi = pa_mainloop_get_api(pa_ml);
+    pa_context = pa_context_new(pa_mlapi, "Volume Status Monitor");
 
-// ... (Initialization of 'pa_context' as shown earlier)
+    if (pa_context_connect(pa_context, NULL, 0, NULL) < 0) {
+        fprintf(stderr, "Failed to connect to PulseAudio: %s\n", pa_strerror(error));
+        return 1; // Handle error appropriately
+    }
 
-// Subscribe to volume change events
-pa_subscription_mask_t mask = PA_SUBSCRIPTION_MASK_SINK;  
-pa_operation *op = pa_context_subscribe(pa_context, mask, volume_change_callback, NULL);
+    // Subscribe to volume change events
+    pa_subscription_mask_t mask = PA_SUBSCRIPTION_MASK_SINK;  
+    pa_operation *op = pa_context_subscribe(pa_context, mask, volume_change_callback, NULL);
+    pa_operation_unref(op);
 
-// ... wait for events (using your main loop) ...
+    // Start the PulseAudio main loop
+    int ret;  
+    if (pa_mainloop_run(pa_ml, &ret) < 0) {
+        fprintf(stderr, "pa_mainloop_run() failed: %s\n", pa_strerror(error));
+        return 1; 
+    }
 
-// Cleanup
-pa_operation_unref(op); // Unsubscribe
-pa_context_disconnect(pa_context);
-pa_context_unref(pa_context);
-pa_mainloop_free(pa_ml);
+    // PulseAudio cleanup
+    pa_operation_unref(op); // Unsubscribe
+    pa_context_disconnect(pa_context);
+    pa_context_unref(pa_context);
+    pa_mainloop_free(pa_ml);
 
-#endif
+    return 0;
+} 
+
